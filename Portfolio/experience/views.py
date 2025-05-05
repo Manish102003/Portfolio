65956741f117a0a2ea3556bb15de7bc0c6515fe0
Manish Kumar <ys3365143@gmail.com>
2025-05-05 18:00:23 +0530

    Completed Home Page

--- Portfolio/experience/views.py
+++ Portfolio/experience/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def experience(request):
-    return render(request,'layout.html')
+    return render(request,'experience.html')
--- Portfolio/projects/views.py
+++ Portfolio/projects/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 # Create your views here.
 def projects(request):
-    return render(request,'layout.html')
+    return render(request,'projects.html')
